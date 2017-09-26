04840ef1fc5d534c3fdbeb96f64cbd13fcd1e3d9
keith(mac) <ka33145@googlemail.com>
2017-09-26 19:48:01 +0100

    Added resetTemp test and refactored spec/ThermostatSpec.js

--- spec/ThermostatSpec.js
+++ spec/ThermostatSpec.js
@@ -1,27 +1,38 @@
-describe("Thermostat", function() {
+describe("Thermostat", function () {
 
   var myThermostat;
 
-  beforeEach(function() {
+  beforeEach(function () {
     myThermostat = new Thermostat();
-  })
+  });
 
-  describe("getTemp", function() {
-    it("returns default temperature of 20", function() {
-      expect(myThermostat.getTemp()).toEqual(20);
-    })
-    it("increase temperature by 4", function() {
+  describe("getTemp", function () {
+    it("returns default temperature of " + DEFAULT_TEMP, function () {
+      expect(myThermostat.getTemp()).toEqual(DEFAULT_TEMP);
+    });
+  });
+  describe("increaseTemp", function () {
+    it("increase temperature by 4", function () {
       myThermostat.increaseTemp(4);
       expect(myThermostat.getTemp()).toEqual(24);
-    })
-    it("decrease temperature by 4", function() {
+    });
+  });
+  describe("decreaseTemp", function () {
+    it("decrease temperature by 4", function () {
+
       myThermostat.decreaseTemp(4);
       expect(myThermostat.getTemp()).toEqual(16);
-    })
-  })
-  describe("getPowerStatus", function() {
-    it("return power saving mode", function() {
+    });
+  });
+  describe("getPowerStatus", function () {
+    it("return power saving mode", function () {
       expect(myThermostat.getPowerStatus()).toEqual('on');
-    })
-  })
-})
+    });
+  });
+  describe("resetTemp", function () {
+    it("set tempeature back to default (" + DEFAULT_TEMP + ")", function () {
+      myThermostat.resetTemp();
+      expect(myThermostat.getTemp()).toEqual(DEFAULT_TEMP);
+    });
+  });
+});
